069ff711c566e8ecba6c9ee80b72391c46a6b088
William Holroyd <wholroyd@gmail.com>
2015-06-20 21:39:37 -0400

    Working on getting Express to expose routes if used as module

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
-var express = require('express');
-var app = express();
-var router = module.exports = express.Router();
-
-router.get('/', function(req, res) {
-    res.send('This is the client endpoint');  
-});
-
-// Configure routing
-app.use('/', router);
-
-// Configure some error handlers
-
-// -- register 404 handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// -- register 500 handler (dev)
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    if (process.env.ENVIRONMENT == 'production') {
-        res.render('An error has occured.');
-    } else {    
-        res.render('An error has occured:', {
-            message: err.message,
-            error: err
-        });
-    }
-});
-
-// Start the server
-var port = process.env.PORT || 3000;
-var server = app.listen(port , function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Listening at http://%s:%s using NodeJS verison %s', host, port, process.version);
+module.exports = (function() {
     
-});
+    var express = require('express');
+    var app = express();
+    
+    var handlebars = require('express-handlebars');
+    app.engine('hbs', handlebars({defaultLayout: 'single', extname: '.hbs'}));
+    app.set('view engine', 'hbs');
+    app.use('/assets', express.static('assets'));
+
+    app.get('/', function(req, res) { 
+        con.handlebars(__dirname + '/views/index.hbs', function(err, html){ 
+            res.write(html);
+            res.end();
+        });
+    });
+    
+    // -- register 404 handler
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found from client');
+        err.status = 404;
+        next(err);
+    });
+
+    // -- register 500 handler (dev)
+    app.use(function (err, req, res, next) {
+        if (process.env.ENVIRONMENT == 'production') {
+            res.status(500).send('An error has occured.');
+        } else {
+            res.status(err.status).send('An error has occured:', {
+                message: err.message,
+                error: err
+            });
+        }
+    });
+    
+    return app;
+    
+})();
